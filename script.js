bf924047538a2a5f189d90c5ff27472e978a6a84
zainabcodes03 <zainabmanzoor9397@gmail.com>
2026-04-29 22:39:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeTheme() {
+  document.body.style.background = "linear-gradient(135deg, #89f7fe, #66a6ff)";
+}
+
+function likePost(){
+  document.getElementById("likeBtn").style.background = "pink";
+}
+
+function darkMode() {
+  document.body.style.background = "#631111";
+  document.body.style.color = "white";
+}
